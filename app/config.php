6c346df1f786c2402e06aad71f838d17aa6911c2
Posi-10 <64515605+Posi-10@users.noreply.github.com>
2021-04-29 23:24:12 -0500

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,5 +1,5 @@
 <?php
     define('NOMBRE_SERVIDOR', 'posiconexion');
-    //define('SERVIDOR', 'http://localhost/SistemasWEB/cuento/');
-    define('SERVIDOR', 'http://localhost/cuento/');
+    define('SERVIDOR', 'http://localhost/SistemasWEB/cuento/');
+    //define('SERVIDOR', 'http://localhost/cuento/');
 ?>
